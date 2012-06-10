e82a4ad1f9124cc73e60f93176dc2f92cceae7fe
Robert DeNicola <thedizzle23@gmail.com>
2012-06-10 11:06:47 -0400

    done

---
+++ SettingsBundleAndDefault/SettingsBundleAndDefaultTests/SettingsBundleAndDefaultTests.h
@@ -0,0 +1,13 @@
+//
+//  SettingsBundleAndDefaultTests.h
+//  SettingsBundleAndDefaultTests
+//
+//  Created by Robert DeNicola on 6/10/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SettingsBundleAndDefaultTests : SenTestCase
+
+@end
---
+++ SettingsBundleAndDefault/SettingsBundleAndDefaultTests/SettingsBundleAndDefaultTests.m
@@ -0,0 +1,32 @@
+//
+//  SettingsBundleAndDefaultTests.m
+//  SettingsBundleAndDefaultTests
+//
+//  Created by Robert DeNicola on 6/10/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "SettingsBundleAndDefaultTests.h"
+
+@implementation SettingsBundleAndDefaultTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SettingsBundleAndDefaultTests");
+}
+
+@end
